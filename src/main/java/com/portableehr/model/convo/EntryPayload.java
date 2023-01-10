bc2b554a3522a99bda9c79f3e54da8ec4e391302
Manuel Daniel <manuel.daniel@portableehr.com>
2023-01-10 14:01:27 -0500

    Default behaviour for empty payloads

--- src/main/java/com/portableehr/model/convo/EntryPayload.java
+++ src/main/java/com/portableehr/model/convo/EntryPayload.java
@@ -3,7 +3,7 @@ package com.portableehr.model.convo;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, defaultImpl = Void.class)
 @JsonSubTypes( {
         @JsonSubTypes.Type(EntryPayloadMessage.class),
         @JsonSubTypes.Type(EntryPayloadMove.class),
