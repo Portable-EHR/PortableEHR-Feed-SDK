39514c108c7d2f189b634d20f8542802f64827bd
Manuel Daniel <manuel.daniel@portableehr.com>
2025-06-18 14:20:24 -0400

    Blocked :: add pull entry points parameter clientId

--- src/main/java/com/portableehr/network/server/request/convo/entrypoint/EntryPointPullBundleParameters.java
+++ src/main/java/com/portableehr/network/server/request/convo/entrypoint/EntryPointPullBundleParameters.java
@@ -9,15 +9,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.portableehr.model.LanguageEnum;
 import com.portableehr.network.server.request.convo.ConvoPullSingleRequest;
 
+import java.util.UUID;
+
 /**
  * The parameters for the {@link ConvoPullSingleRequest}
  * <pre><code>
  * {
+ *     "clientId": "the id of the client pulling the entry points",
  *     "lang": {@link LanguageEnum}
  * }
  * </code></pre>
  */
 public class EntryPointPullBundleParameters {
+
+    @JsonProperty("clientId")
+    private UUID clientId = null;
 
     @JsonProperty("lang")
     private LanguageEnum lang = null;
@@ -25,8 +31,17 @@ public class EntryPointPullBundleParameters {
     public EntryPointPullBundleParameters() {
     }
 
-    public EntryPointPullBundleParameters(LanguageEnum lang) {
+    public EntryPointPullBundleParameters(UUID clientId, LanguageEnum lang) {
+        this.clientId = clientId;
         this.lang = lang;
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
     }
 
     public LanguageEnum getLang() {
